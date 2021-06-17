ff23d008ebb1fb2253120e01d904e5daa1c882c3
bettergrammer <wiesnerjjason@gmail.com>
2021-06-17 16:28:53 -0700

    Update package.json

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const Path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.use(express.static(Path.join(`${__dirname}/../public`)));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+if (PORT == null || PORT == "") {
+  PORT = 3000;
+}
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });
