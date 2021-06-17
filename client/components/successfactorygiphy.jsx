fe69302eb70f27d4e6e7094a767eb5fc16d0dd33
bettergrammer <wiesnerjjason@gmail.com>
2021-06-16 19:41:41 -0700

    Update module styling

--- client/components/successfactorygiphy.jsx
+++ client/components/successfactorygiphy.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function SuccessFactoryGiphy() {
     return (
         <div className="giphy-div">
-            <iframe src="http://giphy.com/embed/eTItUowa7HMtYJGmIg" width="100%" height="100%" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
+            <iframe src="https://giphy.com/embed/NDDkrpt4WnvHaMdefj" width="100%" height="100%" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
         </div>
     );
 };
