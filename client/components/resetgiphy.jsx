73397fd561834b611e122cd669bfcd707d8e82fa
bettergrammer <wiesnerjjason@gmail.com>
2021-06-22 16:25:27 -0700

    Update reset giphy

--- client/components/resetgiphy.jsx
+++ client/components/resetgiphy.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function ResetGiphy() {
     return (
         <div className="giphy-div">
-            <iframe src="https://giphy.com/embed/wUDjr9F6zNKKF1Vc2M" width="100%" height="100%" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
+            <iframe src="https://giphy.com/embed/wUDjr9F6zNKKF1Vc2M" frameBorder="1" className="giphy-embed" allowFullScreen></iframe>
         </div>
     );
 }
