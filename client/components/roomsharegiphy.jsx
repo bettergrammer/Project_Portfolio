2f2293a802d02181171f4794230bd897e9f83c70
bettergrammer <wiesnerjjason@gmail.com>
2021-06-22 15:42:12 -0700

    update iframe

--- client/components/roomsharegiphy.jsx
+++ client/components/roomsharegiphy.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function RoomShareGiphy() {
     return (
         <div className="giphy-div">
-            <iframe src="https://giphy.com/embed/Q8Hmu1z4kFN7sNpJoT" width="100%" height="100%" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
+            <iframe src="https://giphy.com/embed/Q8Hmu1z4kFN7sNpJoT" scrolling="no" width="100%" height="100%" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
         </div>
     );
 };
