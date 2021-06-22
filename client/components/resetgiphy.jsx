e3c217a99a1f58827087b7b1a6f3abd9a7538ef5
bettergrammer <wiesnerjjason@gmail.com>
2021-06-22 16:27:45 -0700

    Update giphy-div height

--- client/components/resetgiphy.jsx
+++ client/components/resetgiphy.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function ResetGiphy() {
     return (
         <div className="giphy-div">
-            <iframe src="https://giphy.com/embed/wUDjr9F6zNKKF1Vc2M" frameBorder="1" className="giphy-embed" allowFullScreen></iframe>
+            <iframe src="https://giphy.com/embed/wUDjr9F6zNKKF1Vc2M" className="giphy-embed" allowFullScreen></iframe>
         </div>
     );
 }
