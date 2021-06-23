bb291edc90cbe774fbdb82d2dadefe209c92b268
bettergrammer <wiesnerjjason@gmail.com>
2021-06-22 17:22:30 -0700

    Update roomshare2.0 gif

--- client/components/roomsharegiphy.jsx
+++ client/components/roomsharegiphy.jsx
@@ -6,7 +6,8 @@ export default function RoomShareGiphy() {
         //     <iframe src="https://giphy.com/embed/Q8Hmu1z4kFN7sNpJoT" height="5px" width="5px" scrolling="no" className="giphy-embed" allowFullScreen></iframe>
         // </div>
         <div>
-            <div style={{width:"100%", height:"0", paddingBottom:"50%", position:"relative"}}><iframe src="https://giphy.com/embed/Q8Hmu1z4kFN7sNpJoT" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/Q8Hmu1z4kFN7sNpJoT">via GIPHY</a></p>
+            <div style={{width:"100%", height:"0", paddingBottom:"50%", position:"relative"}}>
+                <iframe src="https://giphy.com/embed/Q8Hmu1z4kFN7sNpJoT" width="100%" height="100%" style={{position:"absolute"}} frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/Q8Hmu1z4kFN7sNpJoT">via GIPHY</a></p>
         </div>
     );
 };
