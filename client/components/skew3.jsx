99e2690b8f4134222b635ed12e71ee1baec9d99e
bettergrammer <wiesnerjjason@gmail.com>
2021-09-19 13:14:38 -0700

    Update skew3

--- client/components/skew3.jsx
+++ client/components/skew3.jsx
@@ -5,11 +5,11 @@ export default function Skew3() {
         <div className="wrapper">
             <div id="skew3" className="cus-div">
                 <div className="content">
-                    <h3>True Agile Developer, Motivated To Collaborate With Others & Produce Results</h3>
+                    <h3>True Agile Developer, Motivated To Collaborate With Others & Produce Results:</h3>
                     <p>
                         With real world experience on an agile team, I combine my passion for continuous iteration and new technology to create efficient, performant code that constantly improves products.
                     </p>
-                    <h2>Contact</h2>
+                    <h2>Contact:</h2>
                     <ul>
                         <li>wiesnerjjason@gmail.com</li>
                         <li>(619)746-2897</li>
